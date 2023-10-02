058a22933843053f7dd083ffe5f53fef3654b04c
boldi-kdg <boldizsar.olajos@student.kdg.be>
2023-10-02 16:14:52 +0200

    Working sensor arduino part

--- Spike-Acoustics/src/main.cpp
+++ Spike-Acoustics/src/main.cpp
@@ -3,12 +3,13 @@
 #define SOUND_ANALOG_PIN A0
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Hello world!");
+  Serial.begin(115200);
   pinMode(SOUND_ANALOG_PIN, INPUT);
 }
 
 void loop() {
-  Serial.print(analogRead(SOUND_ANALOG_PIN)); Serial.print("/1023\n");
-  delay(10);
+  int val = analogRead(SOUND_ANALOG_PIN);
+  Serial.print(val); Serial.print("\n");
+  delayMicroseconds(20);
 }
+
