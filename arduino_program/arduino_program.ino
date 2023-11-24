f9bdb0e3f198b820a0ce90820a0f361983a6438b
boldi-kdg <boldizsar.olajos@student.kdg.be>
2023-11-24 14:58:38 +0100

    Remove commented out code, add some meaningful comments...

--- arduino_program/arduino_program.ino
+++ arduino_program/arduino_program.ino
@@ -1,31 +1,26 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(2,3);
+SoftwareSerial espSerial(2,3); 
+/* 
+  The Serial is the connection to a computer if there is one connected to the arduino via the USB port.
+  The espSerial is the serial connection to the ESP wifi module
+  The arduino acts as a "man in the middle" and transmits all data coming from one direction to the other one and vice versa
+  The arduino will (later) listen to (a very limited set of) commands, but only from the esp, so that we can make the RGB led change color, or to activate
+  a relay when the system deems it necessary (for ventilation).
+ */
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(57600);
-  mySerial.begin(57600);
-  mySerial.listen();
-
+  espSerial.begin(57600);
+  espSerial.listen();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  String incomingString="";
-  boolean stringReady = false;
 
-  while(mySerial.available()) {
-    Serial.print((char) mySerial.read());
-    //incomingString += (char) mySerial.read();
-    //stringReady = true;
+  while(espSerial.available()) {
+    Serial.print((char) espSerial.read());
   }
 
   while(Serial.available()) {
-    mySerial.print((char) Serial.read());
-    //incomingString += (char) mySerial.read();
-    //stringReady = true;
+    espSerial.print((char) Serial.read());
   }
-  //if (stringReady) {
-    //Serial.println("Recieved String: " + incomingString);
-  //}
 }
